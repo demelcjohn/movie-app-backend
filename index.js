dd77e2d32484fae9820b1d3d482fc09ecfc7ed79
Demel C <98221839+demelcjohn@users.noreply.github.com>
2023-03-27 13:05:26 +0000

    update 2

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import app from "./server.js"
 import mongodb from "mongodb"
-import ReviewsDAO from "./dao/reviewsDAO.js"
+//import ReviewsDAO from "./dao/reviewsDAO.js"
 
 const MongoClient = mongodb.MongoClient
 const mongo_username = process.env['MONGO_USERNAME']
 const mongo_password = process.env['MONGO_PASSWORD']
-const uri = `mongodb+srv://${mongo_username}:${mongo_password}>@cluster0.agncvbo.mongodb.net/retryWrites=true&w=majority`
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.6moljfw.mongodb.net/?retryWrites=true&w=majority`
 
 const port = 8000
 
@@ -19,6 +19,11 @@ MongoClient.connect(
 .catch(err=>{
   console.error(err.stack)
   process.exit(1)
+})
+.then(async client => {
+  app.listen(port,() => {
+    console.log(`listening on port ${port}`)
+  })
 })
 
 
