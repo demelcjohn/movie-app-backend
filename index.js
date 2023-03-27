b2c07b29e9409153c7c02c5b67a1a2aa803e22f8
Demel C <98221839+demelcjohn@users.noreply.github.com>
2023-03-27 01:37:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import app from "./server.js"
+import mongodb from "mongodb"
+import ReviewsDAO from "./dao/reviewsDAO.js"
+
+const MongoClient = mongodb.MongoClient
+const mongo_username = process.env['MONGO_USERNAME']
+const mongo_password = process.env['MONGO_PASSWORD']
+const uri = `mongodb+srv://${mongo_username}:<password>@cluster0.agncvbo.mongodb.net/?retryWrites=true&w=majority`
+
+
+
+
+
+const mySecret = process.env['MONGO_USERNAME']
