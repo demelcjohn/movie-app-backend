b4782add829141865e297ebd47a1b99e04feb34a
Demel C <98221839+demelcjohn@users.noreply.github.com>
2023-03-27 01:48:54 +0000

    update 1

--- index.js
+++ index.js
@@ -5,7 +5,21 @@ import ReviewsDAO from "./dao/reviewsDAO.js"
 const MongoClient = mongodb.MongoClient
 const mongo_username = process.env['MONGO_USERNAME']
 const mongo_password = process.env['MONGO_PASSWORD']
-const uri = `mongodb+srv://${mongo_username}:<password>@cluster0.agncvbo.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}>@cluster0.agncvbo.mongodb.net/retryWrites=true&w=majority`
+
+const port = 8000
+
+MongoClient.connect(
+  uri,
+  {
+    maxPoolSize: 50,
+    wtimeoutMS: 2500,
+    useNewUrlParser: true
+  })
+.catch(err=>{
+  console.error(err.stack)
+  process.exit(1)
+})
 
 
 
