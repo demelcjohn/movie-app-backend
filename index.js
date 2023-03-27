2703f6b6fcccf204ecc1f4ee9893a3350d71da98
Demel C <98221839+demelcjohn@users.noreply.github.com>
2023-03-27 14:55:11 +0000

    final backend commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import app from "./server.js"
 import mongodb from "mongodb"
-//import ReviewsDAO from "./dao/reviewsDAO.js"
+import ReviewsDAO from "./dao/reviewsDAO.js"
 
 const MongoClient = mongodb.MongoClient
 const mongo_username = process.env['MONGO_USERNAME']
@@ -21,6 +21,7 @@ MongoClient.connect(
   process.exit(1)
 })
 .then(async client => {
+  await ReviewsDAO.injectDB(client)
   app.listen(port,() => {
     console.log(`listening on port ${port}`)
   })
